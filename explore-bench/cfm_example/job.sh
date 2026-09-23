36a5c3af8912013fee2c581065d4be67634f47b8
alopezcordero <alejandro.lopezcordero01@utrgv.edu>
2026-09-23 01:22:30 -0500

    changed to kimq

--- explore-bench/cfm_example/job.sh
+++ explore-bench/cfm_example/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=CFM-ExploreBench
-#SBATCH --partition=gpul40q
-#SBATCH --time=24:00:00
+#SBATCH --partition=kimq
+#SBATCH --time=12:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1 
